d5bbeb886aecb043b18c6518c5c7e18f08008afd
projectsharemarket <150452493+projectsharemarket@users.noreply.github.com>
2023-11-28 14:58:31 +0900

    Update client.js

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
-const client = axios.create();
+//const client = axios.create();
+const client = axios.create({
+  baseURL: 'https://port-0-server-cn1vmr2clp99vkac.sel5.cloudtype.app/',
+});
+
+client.defaults.headers.common['Authorization'] = 'Bearer 33e86ac48e993d06bb3afd382863457e6bca79daf983ec4ba783e75999b1f4a000e686223b87f05659f7f85e0d369a596d32d2df6f76c41fca708c23a7c15488';
 
 export default client;
